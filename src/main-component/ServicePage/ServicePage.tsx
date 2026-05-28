6ee8df0caa197a1e89b55ddc3258345d12947c7b
malinga-PW <pixelwave.lk@gmail.com>
2026-05-29 02:31:39 +0530

    Update ServicePage.tsx

--- src/main-component/ServicePage/ServicePage.tsx
+++ src/main-component/ServicePage/ServicePage.tsx
@@ -1,4 +1,5 @@
-import React, { Fragment } from "react";
+import React, { Fragment, useEffect } from "react";
+
 import Header from "../../components/Header/Header";
 import PageTitle from "../../components/pagetitle/PageTitle";
 import ServiceSection from "./ServiceSection";
@@ -6,19 +7,44 @@ import Scrollbar from "../../components/scrollbar/scrollbar";
 import Footer from "../../components/footer/Footer";
 
 const ServicePage: React.FC = () => {
+
+  useEffect(() => {
+    document.title = "Business Solutions | PixelWave Total Solution";
+  }, []);
+
   return (
     <Fragment>
+
       <div className='about-page inner-page'>
+
         <div className="body_wrap o-clip">
+
+          {/* Header */}
           <Header />
+
           <main>
-            <PageTitle pageTitle="Services" pagesub="Services" />
+
+            {/* Page Title */}
+            <PageTitle
+              pageTitle="Business Growth Solutions"
+              pagesub="Solutions"
+            />
+
+            {/* Services */}
             <ServiceSection />
+
           </main>
+
+          {/* Footer */}
           <Footer />
+
+          {/* Scrollbar */}
           <Scrollbar />
+
         </div>
+
       </div>
+
     </Fragment>
   );
 };
